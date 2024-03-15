a12a0ef2cc460860653c51251eaf1f5848f2f6c4
gsbad <gsbadbr@gmail.com>
2024-03-15 03:36:58 -0300

    Criado app users, configurado urls.py e implementada a funçao login de usuarios

--- learningLog/learningLog/settings.py
+++ learningLog/learningLog/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Minha app referenciada
-    'learningLogApp'
+    # Minhas apps referenciadas
+    'learningLogApp',
+    'users'
 ]
 
 MIDDLEWARE = [
