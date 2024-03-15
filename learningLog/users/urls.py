4ac13f56a630025a255cc098953078901c573c49
gsbad <gsbadbr@gmail.com>
2024-03-15 16:27:27 -0300

    Implementação do Logout

--- learningLog/users/urls.py
+++ learningLog/users/urls.py
@@ -1,10 +1,11 @@
 """Define padrões de URL para users"""
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 app_name = "users"
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/' , LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='learningLogApp:index'), name='logout'),
 ]
