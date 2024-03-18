96f2e542ff1ee25087d51d170ca7698049bdd2cc
gsbad <gsbadbr@gmail.com>
2024-03-18 09:32:25 -0300

    protegendo função nova_entrada de usuarios que nao sao os donos do assunto

--- learningLog/learningLogApp/views.py
+++ learningLog/learningLogApp/views.py
@@ -45,7 +45,7 @@ def novo_assunto(request):
         novo_assunto.owner = request.user
         novo_assunto.save()
         return HttpResponseRedirect(reverse('learningLogApp:assuntos'))
-    #atraves do contexto passa o forumario que recebeu a instancia da class AssuntoForm() de forms.py e usada com tag de template {{ form.as_p }}
+    #atraves do contexto passa o formulario que recebeu a instancia da class AssuntoForm() de forms.py e usada com tag de template {{ form.as_p }}
     contexto = { 'form': form }
     return render(request, 'learningLogApp/novo_assunto.html', contexto)
 
@@ -53,6 +53,12 @@ def novo_assunto(request):
 def nova_entrada(request , assunto_id):
     """Adiciona uma nova entrada."""
     assunto = Assunto.objects.get(id = assunto_id)
+    
+    owner_assunto = assunto.owner
+    owner_request = request.user
+    # Garante que o assunto pertence ao usuario atual
+    if owner_assunto != owner_request:
+        raise Http404     
 
     if request.method != 'POST':
         # Nenhum dado submetido; cria um formulário em branco
@@ -62,8 +68,10 @@ def nova_entrada(request , assunto_id):
         form = EntradaForm(data = request.POST)
     
     if form.is_valid():
+        # proteçao de novas entradas            
+        nova_entrada.owner = request.user       
         nova_entrada = form.save(commit = False)
-        nova_entrada.assunto = assunto
+        nova_entrada.assunto = assunto           
         nova_entrada.save()
         return HttpResponseRedirect(reverse('learningLogApp:assunto',
                                             args=[assunto_id]))
