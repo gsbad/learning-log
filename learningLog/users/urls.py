6c812e594759e1cb1fc51da2ffc36b59a8052726
gsbad <gsbadbr@gmail.com>
2024-03-17 23:03:36 -0300

    implementada a funçao de registro de novo usuário (registrar)

--- learningLog/users/urls.py
+++ learningLog/users/urls.py
@@ -3,9 +3,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+# Importa o modúlo viws para a pasta do app "."
+from . import views
+
 app_name = "users"
 
 urlpatterns = [
     path('login/' , LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='learningLogApp:index'), name='logout'),
+    path('registrar/', views.registrar, name='registrar'),
+
 ]
