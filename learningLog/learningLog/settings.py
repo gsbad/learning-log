47bde664ecd1ccbac2ba05c4b40e36dabd491738
gsbad <gsbadbr@gmail.com>
2024-03-21 17:53:27 -0300

    mais configuraçoes do STATIC

--- learningLog/learningLog/settings.py
+++ learningLog/learningLog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&!-d(0z+c_g&o6t*+plou4aukdhvj4g4v81)*73mo5u-qlvnyr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost' , '127.0.0.1']
 
@@ -142,7 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
