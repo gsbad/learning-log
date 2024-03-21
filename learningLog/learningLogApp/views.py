f5455f3fec884eb5d4121e9818363b2394e17099
gsbad <gsbadbr@gmail.com>
2024-03-21 01:27:58 -0300

    mudanças no index com adiçao de cards mostrando ultimos assuntos e usuarios. Implementaçao de componente de paginaçao

--- learningLog/learningLogApp/views.py
+++ learningLog/learningLogApp/views.py
@@ -1,58 +1,135 @@
+# Importações necessárias para views, models e forms
 from django.shortcuts import render, get_object_or_404
-
-from .models import Assunto, Entrada
-from .forms import AssuntoForm , EntradaForm
-
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
-
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
-# Django Rest Framework imports
+from .models import Assunto, Entrada, User
+from .forms import AssuntoForm, EntradaForm
+
+# Importações do Django Rest Framework para criação de APIs
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from .serializers import AssuntoSerializer, EntradaSerializer
 
+# ViewSets do Django Rest Framework para os modelos Assunto e Entrada
 class AssuntoViewSet(viewsets.ModelViewSet):
+    """
+    Fornece as ações de listagem, criação, atualização e exclusão para o modelo Assunto.
+    Utiliza DjangoFilterBackend para filtrar os assuntos com base em determinados campos.
+    """
     queryset = Assunto.objects.all()
     serializer_class = AssuntoSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'texto', 'data_inicial', 'owner']
     
-
 class EntradaViewSet(viewsets.ModelViewSet):
+    """
+    Fornece as ações de listagem, criação, atualização e exclusão para o modelo Entrada.
+    Utiliza DjangoFilterBackend para filtrar as entradas com base em determinados campos.
+    """
     queryset = Entrada.objects.all()
     serializer_class = EntradaSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'assunto', 'texto', 'data_inicial']
 
+# View para a página inicial
 def index(request):
-    """A página inicial de Learning Log"""
-    return render(request, 'learningLogApp/index.html')
+    """
+    Exibe a página inicial do aplicativo.
+    Lista os últimos assuntos públicos e usuários cadastrados, ambos paginados.
+    """
+    # Paginação para os últimos assuntos públicos
+    assuntos_publicos_list = Assunto.objects.filter(public=True).order_by('-data_inicial')
+    paginator_assuntos_publicos = Paginator(assuntos_publicos_list, 5)
+    page_number_assuntos = request.GET.get('page_assuntos')
+    page_obj_assuntos = paginator_assuntos_publicos.get_page(page_number_assuntos)
 
-#@login_required
+    # Paginação para os usuários cadastrados
+    usuarios_list = User.objects.all().order_by('-date_joined')
+    paginator_usuarios = Paginator(usuarios_list, 5)
+    page_number_usuarios = request.GET.get('page_usuarios')
+    page_obj_usuarios = paginator_usuarios.get_page(page_number_usuarios)
+
+    # Combina os objetos de página no contexto para renderização
+    context = {
+        'page_obj_assuntos': page_obj_assuntos,
+        'page_obj_usuarios': page_obj_usuarios,
+    }
+
+    return render(request, 'learningLogApp/index.html', context)
+
+@login_required
 def assuntos(request):
-    """Página de assuntos"""
+    """
+    Exibe uma lista de assuntos. Para usuários autenticados, mostra seus assuntos e assuntos públicos.
+    Para usuários não autenticados, mostra apenas assuntos públicos.
+    """
     if request.user.is_authenticated:
         assuntos = Assunto.objects.filter(owner=request.user) | Assunto.objects.filter(public=True)
     else:
         assuntos = Assunto.objects.filter(public=True)
-    contexto = {'assuntos': assuntos}   
-    """A página inicial de Learning Log"""
+    contexto = {'assuntos': assuntos}
     return render(request, 'learningLogApp/assuntos.html', contexto)
 
-#@login_required
+@login_required
 def assunto(request, assunto_id):
-    """Página de assuntos"""
+    """
+    Exibe detalhes de um assunto específico, incluindo suas entradas associadas.
+    Verifica se o assunto é público ou se pertence ao usuário antes de exibir.
+    """
     assunto = get_object_or_404(Assunto, id=assunto_id)
-
-    # Se o assunto for privado e o usuário não for o proprietário, levanta 404.
     if not assunto.public and assunto.owner != request.user:
         raise Http404
-    
     entradas = assunto.entrada_set.order_by('-data_inicial')
-    contexto = {'assunto' : assunto, 'entradas' : entradas}
+    contexto = {'assunto': assunto, 'entradas': entradas}
     return render(request, 'learningLogApp/assunto.html', contexto)
+
+# Seguem as demais views para manipulação de assuntos e entradas, aplicando lógica similar
+# com verificações de propriedade e permissões, tratamento de formulários e redirecionamentos.
+
+@login_required
+def novo_assunto(request):
+    """
+    Permite ao usuário logado criar um novo assunto.
+    Utiliza o método POST para submissão do formulário. Um formulário em branco é exibido inicialmente.
+    """
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco.
+        form = AssuntoForm()
+    else:
+        # Dados POST submetidos; processa os dados.
+        form = AssuntoForm(request.POST)
+        if form.is_valid():
+            novo_assunto = form.save(commit=False)
+            novo_assunto.owner = request.user  # Atribui o assunto ao usuário atual.
+            novo_assunto.save()
+            return HttpResponseRedirect(reverse('learningLogApp:assuntos'))
+
+    contexto = {'form': form}
+    return render(request, 'learningLogApp/novo_assunto.html', contexto)
+
+@login_required
+def editar_assunto(request, assunto_id):
+    """
+    Permite ao usuário editar um assunto existente.
+    Verifica se o assunto pertence ao usuário antes de permitir a edição.
+    """
+    assunto = get_object_or_404(Assunto, id=assunto_id)
+    if assunto.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        form = AssuntoForm(instance=assunto)  # Preenche o formulário com o assunto atual.
+    else:
+        form = AssuntoForm(instance=assunto, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learningLogApp:assunto', args=[assunto.id]))
+
+    contexto = {'assunto': assunto, 'form': form}
+    return render(request, 'learningLogApp/editar_assunto.html', contexto)
 
 @login_required
 def excluir_assunto(request, assunto_id):
@@ -64,101 +141,62 @@ def excluir_assunto(request, assunto_id):
     assunto.delete()
     return HttpResponseRedirect(reverse('learningLogApp:assuntos'))
 
-@login_required
-def novo_assunto(request):
-    """Adiciona um novo assunto."""
-    if request.method != 'POST':
-        # Nenhum dado submetido; cria um formulário em branco
-        form = AssuntoForm()
-    else:
-        # Dados de POST submetidos; processa os dados
-        form = AssuntoForm(request.POST)
-    
-    if form.is_valid():
-        novo_assunto = form.save(commit = False)
-        novo_assunto.owner = request.user
-        novo_assunto.save()
-        return HttpResponseRedirect(reverse('learningLogApp:assuntos'))
-    #atraves do contexto passa o formulario que recebeu a instancia da class AssuntoForm() de forms.py e usada com tag de template {{ form.as_p }}
-    contexto = { 'form': form }
-    return render(request, 'learningLogApp/novo_assunto.html', contexto)
 
 @login_required
-def editar_assunto(request, assunto_id):
+def nova_entrada(request, assunto_id):
+    """
+    Permite ao usuário adicionar uma nova entrada para um assunto específico.
+    Garante que o assunto pertence ao usuário antes de permitir a adição da entrada.
+    """
     assunto = get_object_or_404(Assunto, id=assunto_id)
+    if assunto.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
-        # Preenche o formulário com a instância atual.
-        form = AssuntoForm(instance=assunto)
+        form = EntradaForm()  # Formulário em branco inicialmente.
     else:
-        # POST, processar dados do formulário.
-        form = AssuntoForm(instance=assunto, data=request.POST)
+        form = EntradaForm(data=request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('learningLogApp:assunto', id=assunto_id))
+            nova_entrada = form.save(commit=False)
+            nova_entrada.assunto = assunto  # Associa a entrada ao assunto correto.
+            nova_entrada.save()
+            return HttpResponseRedirect(reverse('learningLogApp:assunto', args=[assunto_id]))
 
     contexto = {'assunto': assunto, 'form': form}
-    return render(request, 'learningLogApp/editar_assunto.html', contexto)
-
-@login_required
-def nova_entrada(request , assunto_id):
-    """Adiciona uma nova entrada."""
-    assunto = get_object_or_404(Assunto,id = assunto_id)
-    
-    owner_assunto = assunto.owner
-    owner_request = request.user
-    # Garante que o assunto pertence ao usuario atual
-    if owner_assunto != owner_request:
-        raise Http404     
-
-    if request.method != 'POST':
-        # Nenhum dado submetido; cria um formulário em branco
-        form = EntradaForm()
-    else:
-        # Dados de POST submetidos; processa os dados
-        form = EntradaForm(data = request.POST)
-    
-    if form.is_valid():
-        # proteçao de novas entradas                 
-        nova_entrada = form.save(commit = False)
-        nova_entrada.assunto = assunto           
-        nova_entrada.save()
-        return HttpResponseRedirect(reverse('learningLogApp:assunto',
-                                            args=[assunto_id]))
-    contexto = { 'assunto' : assunto , 'form': form }
     return render(request, 'learningLogApp/nova_entrada.html', contexto)
 
 @login_required
-def editar_entrada(request , entrada_id):
-    """Editar uma entrada."""
-    entrada = get_object_or_404(Entrada, id = entrada_id)
+def editar_entrada(request, entrada_id):
+    """
+    Permite ao usuário editar uma entrada existente.
+    Verifica se a entrada pertence ao usuário antes de permitir a edição.
+    """
+    entrada = get_object_or_404(Entrada, id=entrada_id)
     assunto = entrada.assunto
-     # Garante que o assunto pertence ao usuario atual
     if assunto.owner != request.user:
-        raise Http404   
+        raise Http404
+
     if request.method != 'POST':
-        # Requisição inicial; Preenche previamente o form com entrada atual
-        form = EntradaForm(instance = entrada)
+        form = EntradaForm(instance=entrada)  # Preenche o formulário com a entrada atual.
     else:
-        # Dados de POST submetidos; processa os dados
-        form = EntradaForm(instance = entrada, data = request.POST)
-    
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect(reverse('learningLogApp:assunto',
-                                            args=[assunto.id]))
-    contexto = { 'entrada' : entrada , 'assunto' : assunto , 'form': form }
+        form = EntradaForm(instance=entrada, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learningLogApp:assunto', args=[assunto.id]))
+
+    contexto = {'entrada': entrada, 'assunto': assunto, 'form': form}
     return render(request, 'learningLogApp/editar_entrada.html', contexto)
 
 @login_required
 def excluir_entrada(request, entrada_id):
-
+    """
+    Permite ao usuário excluir uma entrada existente.
+    Verifica se a entrada pertence ao usuário antes de proceder com a exclusão.
+    """
     entrada = get_object_or_404(Entrada, id=entrada_id)
-    assunto_id = entrada.assunto.id
-
-    # Verifique se o usuário atual é o proprietário da entrada ou do assunto relacionado
     if entrada.assunto.owner != request.user:
         raise Http404
 
-    entrada.delete()
+    assunto_id = entrada.assunto.id
+    entrada.delete()  # Exclui a entrada.
     return HttpResponseRedirect(reverse('learningLogApp:assunto', args=[assunto_id]))
