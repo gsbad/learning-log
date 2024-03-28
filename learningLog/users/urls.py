171f0fc0344e6862ef5a8f608d34ba02734b8e7d
gsbad <gsbadbr@gmail.com>
2024-03-28 16:31:58 -0300

    atualizando .gitignore , criaçao da pagina de usuario para alter perfil e senha

--- learningLog/users/urls.py
+++ learningLog/users/urls.py
@@ -1,6 +1,6 @@
 """Define padrões de URL para users"""
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.urls import path
 
 # Importa o modúlo viws para a pasta do app "."
@@ -11,6 +11,9 @@ app_name = "users"
 urlpatterns = [
     path('login/' , LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='learningLogApp:index'), name='logout'),
-    path('registrar/', views.registrar, name='registrar'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('registrar/', views.registrar, name='registrar'), 
+    path('editar/', views.editar_usuario, name='editar_usuario'),   
+    path('mudar_senha/', PasswordChangeView.as_view(success_url='learningLogApp:index'), name='mudar_senha'),   
 
 ]
--- learningLog/users/views.py
+++ learningLog/users/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from .forms import EditarUsuarioForm
 
 def registrar(request):
     """Faz o cadastro de um novo usuário."""
@@ -20,3 +23,32 @@ def registrar(request):
         return HttpResponseRedirect(reverse('learningLogApp:index'))
     context = {'form': form}
     return render(request, 'users/registrar.html', context)
+
+@login_required
+def perfil(request):
+    return render(request, 'users/perfil.html')
+
+@login_required
+def editar_usuario(request):
+    if request.method == 'POST':
+        form = EditarUsuarioForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:perfil')  # Redirecione para a view de perfil do usuário após sucesso
+    else:
+        form = EditarUsuarioForm(instance=request.user)
+
+    return render(request, 'users/editar_usuario.html', {'form': form})
+
+@login_required
+def mudar_senha(request):
+    if request.method == 'POST':
+        print(request.user)
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Importante para não deslogar o usuário após a mudança de senha
+            return HttpResponseRedirect(reverse('learningLogApp:index'))
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'users/mudar_senha.html', {'form': form})
